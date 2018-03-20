e3c9bd71aec04c353d8c75252e058641566c1ff1
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-03-20 23:54:42 +0300

    code optimization

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
 module.exports = function solveEquation(equation) {
-  var arr=equation.split(' ');
-  var a=0, b=0, c=0;
-  if (arr[0]=='-' && arr[4]=='+' && arr[8]=='+') {
-    a=+arr[1]*(-1);
-    b=+arr[5];
-    c=+arr[9];
-  } else if (arr[0]=='-' && arr[4]=='-' && arr[8]=='+') {
-    a=+arr[1]*(-1);
-    b=+arr[5]*(-1);
-    c=+arr[9];
-  } else if (arr[0]=='-' && arr[4]=='-' && arr[8]=='-') {
-    a=+arr[1]*(-1);
-    b=+arr[5]*(-1);
-    c=+arr[9]*(-1);
-  } else if (arr[0]=='-' && arr[4]=='+' && arr[8]=='-') {
-    a=+arr[1]*(-1);
-    b=+arr[5];
-    c=+arr[9]*(-1);
-  } else if (typeof(+arr[0])=='number' && arr[3]=='+' && arr[7]=='-') {
-    a=+arr[0];
-    b=+arr[4];
-    c=+arr[8]*(-1);
-  } else if (typeof(+arr[0])=='number' && arr[3]=='-' && arr[7]=='+') {
-    a=+arr[0];
-    b=+arr[4]*(-1);
-    c=+arr[8];
-  } else if (typeof(+arr[0])=='number' && arr[3]=='-' && arr[7]=='-') {
-    a=+arr[0];
-    b=+arr[4]*(-1);
-    c=+arr[8]*(-1);
-  } else if (typeof(+arr[0])=='number' && arr[3]=='+' && arr[7]=='+') {
-    a=+arr[0];
-    b=+arr[4];
-    c=+arr[8];
-  }
-
-  var d=b*b-4*a*c;
-  var D=Math.sqrt(d);
-  var x1=Math.round((-b+D)/(2*a));
-  var x2=Math.round((-b-D)/(2*a));
-
+  let arr=equation.split(' ');
+  if (arr[0] != '-') {arr.unshift(' ');}
+  let a=+(arr[0] + arr[1]);
+  let b=+(arr[4] + arr[5]);
+  let c=+(arr[8] + arr[9]);
+  let d=b*b-4*a*c;
+  let D=Math.sqrt(d);
+  let x1=Math.round((-b+D)/(2*a));
+  let x2=Math.round((-b-D)/(2*a));
   if (a<0) {return [x1,x2];
   } else {return [x2,x1];}
 }
